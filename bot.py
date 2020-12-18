308a2a363e8312ad32f314f84044ac5d33289d6f
Whoisadri2 <66048641+Whoisadri2@users.noreply.github.com>
2020-12-18 18:49:22 +0100

    Bot.py Actualizado

--- bot.py
+++ bot.py
@@ -10,10 +10,16 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send("Pong 🏓")
 
 @bot.command()
 async def suma(ctx, numUno: int, numDos: int):
-   await ctx.send(numOne + numTwo)
+   await ctx.send(numUno + numDos)
+
+@bot.command()
+async def infoserver(ctx):
+    embed = discord.Embed(title=f"", description="", color=discord.Color.red())
+    embed.add_field(name="Informacion de el Servidor", value=f"**__Nombre de el Servidor__**: {ctx.guild.name}\n **__ID de el Servidor__**: {ctx.guild.id}\n **__Servidor Creado el__**: {ctx.guild.created_at}\n **__Region de el Servidor__**: {ctx.guild.region}")
+    await ctx.send(embed=embed)
 
 bot.run('')
